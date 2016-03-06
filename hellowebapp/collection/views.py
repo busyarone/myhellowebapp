31e1442db8ad57df397d121a3d3e387c07a3420a
Arone Arokiasami <arone@dropthought.com>
2016-03-06 14:34:02 -0800

    Passing variables to the templates

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    number = 6
+    thing = "Thing name"
+    name = "Arone Arokiasami"
+    return render(request,'index.html',
+                  {'number':number,
+                   'thing':thing,
+                   'name':name})
