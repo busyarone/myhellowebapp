68a11967beaaf0fadb6cb96883c0c0eab0ad5800
Arone Arokiasami <arone@dropthought.com>
2016-03-06 18:38:42 -0800

    Dynamic data by querying DB

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
     number = 6
     thing = "Thing name"
     name = "Arone Arokiasami"
+    things = Thing.objects.all()
     return render(request,'index.html',
                   {'number':number,
                    'thing':thing,
-                   'name':name})
+                   'name':name,
+                   'things':things,})
